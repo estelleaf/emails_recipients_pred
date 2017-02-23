b76d1aa0e7ce17a367f50088d22fc1f44760f935
Estelle Aflalo <estelleaflalo@eduroam-1-115.enst.fr>
2017-02-23 14:13:36 +0100

    -

---
+++ code/init.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 16 15:08:16 2017
+
+@author: pleklol
+"""
+
+# -*- coding: utf-8 -*-
+
+#%%
+import random
+import operator
+import numpy as np
+import pandas as pd
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from numpy.linalg import norm
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+
+
+path_to_data= "/Users/estelleaflalo/Desktop/M2_Data_Science/Second_Period/Text_and_Graph/Project/text_and_graph/Data/"
+#path_to_data = 'C:/Nicolas/M2 MVA/ALTEGRAD/Kaggle/text_and_graph/Data/'
+#path_to_data = '/Users/domitillecoulomb/Documents/DATA_SCIENCE/Semester2/Text_Graph/text_and_graph/Data'
+
+##########################
+# load some of the files #                           
+##########################
+
+
+
+
+training = pd.read_csv(path_to_data + 'training_set.csv', sep=',', header=0)
+
+training_info = pd.read_csv(path_to_data + 'training_info.csv', sep=',', header=0)
+
+test = pd.read_csv(path_to_data + 'test_set.csv', sep=',', header=0)
+
+test_info= pd.read_csv(path_to_data + 'test_info.csv', sep=',', header=0)
+
+def init_dic(training,training_info):
+    emails_ids_per_sender = {}
+    for index, series in training.iterrows():
+        row = series.tolist()
+        sender = row[0]
+        ids = row[1:][0].split(' ')
+        emails_ids_per_sender[sender] = ids
+    
+    # save all unique sender names
+    all_senders = emails_ids_per_sender.keys()
+    
+    # create address book with frequency information for each user
+    address_books = {}
+    i = 0
+    
+    for sender, ids in emails_ids_per_sender.iteritems():
+        recs_temp = []
+        for my_id in ids:
+            recipients = training_info[training_info['mid']==int(my_id)]['recipients'].tolist()
+            recipients = recipients[0].split(' ')
+            # keep only legitimate email addresses
+            recipients = [rec for rec in recipients if '@' in rec]
+            recs_temp.append(recipients)
+        # flatten    
+        recs_temp = [elt for sublist in recs_temp for elt in sublist]
+        # compute recipient counts
+        rec_occ = dict(Counter(recs_temp))
+        # order by frequency
+        sorted_rec_occ = sorted(rec_occ.items(), key=operator.itemgetter(1), reverse = True)
+        # save
+        address_books[sender] = sorted_rec_occ
+        
+        if i % 10 == 0:
+            print i
+        i += 1
+      
+    # save all unique recipient names    
+    all_recs = list(set([elt[0] for sublist in address_books.values() for elt in sublist]))
+    
+    # save all unique user names 
+    all_users = []
+    all_users.extend(all_senders)
+    all_users.extend(all_recs)
+    all_users = list(set(all_users))
+    return all_users, all_senders,all_recs,address_books ,emails_ids_per_sender
+
+
+def split(training,training_info):
+    emails_ids_per_sender = {}
+    for index, series in training.iterrows():
+        row = series.tolist()
+        sender = row[0]
+        ids = row[1:][0].split(' ')
+        emails_ids_per_sender[sender] = ids
+    
+    recs = {}
+    for sender, ids in emails_ids_per_sender.iteritems():
+        recs_temp=[]
+        for my_id in ids:
+            recipients = training_info[training_info['mid']==int(my_id)]['recipients'].tolist()
+            recipients = recipients[0].split(' ')
+            # keep only legitimate email addresses
+            recipients = [rec for rec in recipients if '@' in rec]
+            recs_temp.append(recipients)
+        recs[sender]=recs_temp
+            
+            
+    X_train={} 
+    X_dev={}
+    Y_train={}
+    Y_dev={}
+            
+    for sender, ids in emails_ids_per_sender.iteritems():   
+        X = ids
+        Y = recs[sender]
+        X_train[sender], X_dev[sender], Y_train[sender], Y_dev[sender] = train_test_split(X, Y, test_size=0.2, random_state=42)
+        X_train[sender]=np.array(X_train[sender]).astype('int')
+        X_dev[sender]=np.array(X_dev[sender]).astype('int')
+        
+    return X_train, X_dev, Y_train,Y_dev
+        
+    
+        
---
+++ code/test_tfidf_1_sender.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 11:50:30 2017
+
+@author: estelleaflalo
+"""
+import numpy as np
+from init import split, init_dic
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from numpy.linalg import norm
+from sklearn.metrics.pairwise import linear_kernel
+#from tfidf_centroid import centroid
+
+path_to_data= "/Users/estelleaflalo/Desktop/M2_Data_Science/Second_Period/Text_and_Graph/Project/text_and_graph/Data/"
+#path_to_data = 'C:/Nicolas/M2 MVA/ALTEGRAD/Kaggle/text_and_graph/Data/'
+#path_to_data = '/Users/domitillecoulomb/Documents/DATA_SCIENCE/Semester2/Text_Graph/text_and_graph/Data'
+
+##########################
+# load some of the files #                           
+##########################
+
+
+
+
+training = pd.read_csv(path_to_data + 'training_set.csv', sep=',', header=0)
+
+training_info = pd.read_csv(path_to_data + 'training_info.csv', sep=',', header=0)
+
+test = pd.read_csv(path_to_data + 'test_set.csv', sep=',', header=0)
+
+test_info= pd.read_csv(path_to_data + 'test_info.csv', sep=',', header=0)
+
+
+
+print "Splitting data"
+X_train, X_dev, Y_train,Y_dev=split(training,training_info)
+
+print "Building dictionnaries"
+all_users, all_senders,all_recs,address_books ,emails_ids_per_sender=init_dic(training,training_info)
+
+
+#Select a sender S
+index=1
+sender=all_senders[index]
+X_train_S=X_train[sender]
+X_dev_S=X_dev[sender]
+Y_dev_S=Y_dev[sender]
+Y_train_S=Y_train[sender]
+
+
+##############Create TF IDF vector from mails sent by sender S
+
+
+#vectorize mails sent by a unique sender
+vectorizer_sender = TfidfVectorizer(max_df=0.95,stop_words='english',use_idf=True)
+
+#train
+training_info_S=training_info.loc[training_info['mid'].isin(X_train_S)]
+training_info_S=training_info_S.set_index(np.arange(len(training_info_S)))                                                                 
+training_info_S_mat=training_info_S.as_matrix()
+content_train=training_info_S_mat[:,2]
+
+vec_train = vectorizer_sender.fit_transform(content_train)
+bow_train=vec_train.toarray()
+#test
+test_info_S=training_info.loc[training_info['mid'].isin(X_dev_S)] 
+test_info_S=test_info_S.set_index(np.arange(len(test_info_S)))                                                                 
+test_info_S_mat=test_info_S.as_matrix()
+content_test=test_info_S_mat[:,2]
+
+
+vec_test = vectorizer_sender.transform(content_test)
+bow_test=vec_test.toarray()
+
+
+
+#Creation of centroids for each recipient
+def centroid(sender,dataset_info,bow):
+    df_tfidf = pd.DataFrame(columns=('recipient','tf_idf'))
+    i=0
+    for r in [elt[0] for elt in address_books[sender]]:
+        info_recip_index=dataset_info[dataset_info['recipients'].str.contains(r)].index.tolist() #"rick.dietz@enron.com"
+        bow_recip=bow[info_recip_index]  
+        norma=norm(bow_recip, axis=1, ord=2) 
+        bow_recip_normzd=bow_recip.astype(np.float) / (norma[:,None]+10**(-7))
+        centroid_s_r= np.sum(bow_recip_normzd,axis=0) 
+        df_tfidf.loc[i]  = [r, centroid_s_r]
+        i+=1
+    return df_tfidf
+    
+    
+
+    
+test=centroid(sender,training_info_S,bow_train)
+    
+
+#Similiarity
+cosine_similarities = linear_kernel(mail_test, v).flatten()
+similar_mails = [i for i in cosine_similarities.argsort()[::-1]]
+    
+    
+    
+    
+
